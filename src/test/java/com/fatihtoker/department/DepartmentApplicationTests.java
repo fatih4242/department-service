c8526eeea32e7055d4c7713b0b878dad143ce197
fatih <fatih16@live.nl>
2023-02-07 12:19:55 +0300

    first commit

---
+++ src/test/java/com/fatihtoker/department/DepartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatihtoker.department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
